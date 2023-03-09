febf398e1ae0055adeb208b847043a5d1a7acf1b
usercao <cao858646882@qq.com>
2023-03-09 17:17:03 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,25 @@
-import { defineConfig } from 'vite';
+import { defineConfig, Logger, createLogger } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import { createMpaPlugin } from 'vite-plugin-virtual-mpa';
 import vitePluginRequire from 'vite-plugin-require';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import { fileURLToPath, URL } from 'node:url';
 
+const customLogger = (): Logger => {
+  const logger = createLogger();
+  return {
+    ...logger,
+    warn: (message, options) => {
+      const regexp = new RegExp('files in the public directory are served at the root path.', 'g');
+      if (regexp.test(message)) return;
+      logger.info(message, options);
+    },
+  };
+};
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  customLogger: customLogger(),
   plugins: [
     react(),
     createMpaPlugin({
@@ -15,16 +28,6 @@ export default defineConfig({
       pages: [{ name: 'index', filename: 'index.html' }],
       rewrites: [{ from: /.*/, to: '/public/index.html' }],
     }),
-    // createMpaPlugin({
-    //   verbose: false,
-    //   pages: [
-    //     {
-    //       name: 'index',
-    //       filename: 'index.html',
-    //       template: 'public/index.html',
-    //     },
-    //   ],
-    // }),
     vitePluginRequire(),
     nodePolyfills({ protocolImports: true }),
   ],
